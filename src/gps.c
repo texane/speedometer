f1f76823358ccc5fd04222c26b747e88058d9b1b
texane <texane@gmail.com>
2018-02-06 22:58:04 +0100

    gps_next_gpvtg

--- src/gps.c
+++ src/gps.c
@@ -3,26 +3,53 @@ static void gps_setup(void)
 }
 
 
-static void gps_unit(void)
+static void gps_skip_until(uint8_t c)
 {
-  static uint8_t buf[16];
-  uint8_t i;
   uint8_t x;
-
- redo_line:
-
-  i = 0;
 
   while (1)
   {
-    uart_read_uint8(&x);
-    if (x == '\n') break ;
-    if (i == sizeof(buf)) continue ;
-    buf[i++] = x;
+    if (uart_read_uint8(&x)) continue ;
+    if (x == c) break ;
   }
+}
 
-  uart_write(buf, i);
-  uart_write_rn();
 
-  goto redo_line;
+static void gps_next_gpvtg(uint8_t* buf)
+{
+  uint8_t i;
+  uint8_t c;
+
+ redo_line:
+  gps_skip_until('$');
+
+  uart_read_uint8(&c);
+  if (c != 'G') goto redo_line;
+  uart_read_uint8(&c);
+  if (c != 'P') goto redo_line;
+  uart_read_uint8(&c);
+  if (c != 'V') goto redo_line;
+  uart_read_uint8(&c);
+  if (c != 'T') goto redo_line;
+  uart_read_uint8(&c);
+  if (c != 'G') goto redo_line;
+
+  for (i = 0; i != 7; ++i) gps_skip_until(',');
+
+  uart_read_uint8(&buf[0]);
+  if (buf[0] == ',') goto redo_line;
+  for (i = 1; i != 5; ++i) uart_read_uint8(&buf[i]);
+}
+
+
+static void gps_unit(void)
+{
+  static uint8_t buf[5];
+
+  while (1)
+  {
+    gps_next_gpvtg(buf);
+    uart_write(buf, 5);
+    uart_write_rn();
+  }
 }
